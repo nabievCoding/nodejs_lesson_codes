548e9ab4740ccd442d8def224fd5999118af92c2
NabievCoding <nabievofficial04@gmail.com>
2024-12-31 23:36:20 +0500

    about fs module of nodeJs in inde.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs=require('fs');
+const file=fs.readFile('./index.html',(err,txt)=>{
+        if(err){
+                console.log(err);
+        }
+        else{
+                console.log(txt);
+        }
+})
