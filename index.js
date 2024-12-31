a348e8a0198b5e7e21049e681ce5bafb90ad8f7c
NabievCoding <nabievofficial04@gmail.com>
2024-12-31 23:49:18 +0500

    code about http require of nodeJs

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const fs=require('fs');
-const file=fs.readFile('./index.html',(err,txt)=>{
-        if(err){
-                console.log(err);
+const http=require('http');
+const server=http.createServer((req,res)=>{
+        if(req.url=="/"){
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write('<h1>bu bosh sahifa</h1>');
+                res.end();
         }
-        else{
-                console.log(txt);
+        if(req.url=="/service"){
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write('<h1>bu Service sahifa</h1>');
+                res.end();
+        }
+        if(req.url=="/about"){
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write('<h1>bu About sahifa</h1>');
+                res.end();
         }
 })
+const PORT=3000;
+server.listen(PORT, () => {
+        console.log(`Server http://localhost:${PORT} da ishlamoqda`);
+    });
