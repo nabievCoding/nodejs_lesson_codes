fa311b5a1be2f4b8870e06c767c3b7cdb9301189
NabievCoding <nabievofficial04@gmail.com>
2025-01-01 16:04:02 +0500

    baout expressJs coding

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const http=require('http');
-const server=http.createServer((req,res)=>{
-        if(req.url=="/"){
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write('<h1>bu bosh sahifa</h1>');
-                res.end();
-        }
-        if(req.url=="/service"){
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write('<h1>bu Service sahifa</h1>');
-                res.end();
-        }
-        if(req.url=="/about"){
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write('<h1>bu About sahifa</h1>');
-                res.end();
-        }
+const express=require('express');
+const app=express();
+const portfolioArr=['arduino','website','webApp'];
+app.get('/',(req,res)=>{
+        res.send('salom')
 })
-const PORT=3000;
-server.listen(PORT, () => {
-        console.log(`Server http://localhost:${PORT} da ishlamoqda`);
-    });
+app.get('/portfolio',(req,res)=>{
+        res.send()
+})
+app.get('/service',(req,res)=>{
+        res.send('service sahifasi')
+})
+app.get('/portfolio/:id/:year/:month',(req,res)=>{
+        //localhost:port/portfolio/1/2024/4 qilib ma`lumot http so'rov jo'natsak 
+        //bizga {"id":"1","year":"2024","month":"12"} javob qaytaradi
+        // const id=req.params.id;
+        // res.send(req.params);
+       // biz htpp so'rovda ? bilan quiery jo'natsak barcha quiry larni 
+       //req.query bilan beramiz 
+       res.send(req.query); //javob:{"name":"Ali"}
+})
+const port=process.env.PORT||5000;
+app.listen(port,()=>{
+        console.log(`${port} ishladi`);
+})
